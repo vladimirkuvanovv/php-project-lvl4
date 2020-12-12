907581914eb891f91f1646676ade0337d4344f77
Vladimir <vovan986021@gmail.com>
2020-12-12 20:48:07 +0300

    add send email

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Mail\WelcomeMail;
 use App\Providers\RouteServiceProvider;
 use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -64,6 +65,8 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        \Mail::to($data['email'])->send(new WelcomeMail($data));
+
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
---
+++ app/Mail/WelcomeMail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class WelcomeMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $data;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->view('mails.welcome')
+            ->with([
+                'name' => $this->data['name'],
+                'link' => 'http://shrouded-ravine-40892.herokuapp.com/'
+            ]);
+    }
+}
