2c20edb763869018c7c0bb79cfa6be2ded13814a
Vladimir <vovan986021@gmail.com>
2020-12-12 14:34:09 +0300

    add seeders in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\TaskStatus;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -15,6 +14,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
-        $this->call(TaskStatusesSeeder::class);
+        $this->call(
+            [
+                TaskStatusesSeeder::class,
+                LabelSeeder::class,
+                TaskSeeder::class,
+                UserSeeder::class,
+            ]
+        );
     }
 }
