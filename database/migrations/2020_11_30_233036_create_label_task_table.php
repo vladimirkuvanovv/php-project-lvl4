cf49f69f3d9e59a485274d035621a9fd6b7b1ec6
Vladimir <vovan986021@gmail.com>
2020-12-12 14:28:57 +0300

    add migrations

---
+++ database/migrations/2020_11_30_233036_create_label_task_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLabelTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('label_task', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('label_id')->nullable();
+            $table->unsignedBigInteger('task_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('label_task');
+    }
+}
