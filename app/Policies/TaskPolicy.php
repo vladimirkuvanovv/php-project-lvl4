2b0f6fde9d965714610a2281f4aad7719dd75438
Vladimir <vovan986021@gmail.com>
2020-12-19 15:18:39 +0300

    add policy for delete task

---
+++ app/Policies/TaskPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Task;
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class TaskPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Determine whether the user can delete the model.
+     *
+     * @param  \App\Models\User  $user
+     * @param  \App\Models\Task  $task
+     * @return mixed
+     */
+    public function delete(User $user, Task $task)
+    {
+        return $user->id === $task->created_by_id;
+    }
+}
